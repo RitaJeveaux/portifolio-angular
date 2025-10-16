19265fd043eecd62488ff9c60a4342b150e69ba2
RitaJeveaux <ritajeveaux@gmail.com>
2025-10-16 18:06:12 -0300

    finalizando meu primeiro app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,20 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Cabecalho } from './components/cabecalho/cabecalho';
+import { Projetos } from './components/projetos/projetos';
+import { Sobre } from './components/sobre/sobre';
+import { Rodape } from './components/rodape/rodape';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    Cabecalho, 
+    Projetos,
+    Sobre,
+    Rodape 
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
